885633967bfae035555a3445b06f6ef9714dbbb6
Struan Donald <struan@exo.org.uk>
2023-01-05 17:49:48 +0000

    add django debug toolbar

--- ceuk-marking/urls.py
+++ ceuk-marking/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from crowdsourcer import views
 
@@ -47,3 +49,12 @@ urlpatterns = [
         name="authority_question_edit",
     ),
 ]
+
+if settings.DEBUG:  # pragma: no cover
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
