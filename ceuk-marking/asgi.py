aea0f09d1ef98ad3c855d60663bc6cff92b26e69
Struan Donald <struan@exo.org.uk>
2023-01-12 16:19:16 +0000

    fix wsgi setting location

--- ceuk-marking/asgi.py
+++ ceuk-marking/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crowdsourcer.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ceuk-marking.settings")
 
 application = get_asgi_application()
