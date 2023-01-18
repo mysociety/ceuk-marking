e47343973c963b8e8574f3c123b77a4f8aebac63
Struan Donald <struan@exo.org.uk>
2023-01-18 17:43:08 +0000

    basic script to send registration emails to volunteers

---
+++ crowdsourcer/management/commands/send_password_reset_emails.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.http import HttpRequest
+
+
+class Command(BaseCommand):
+    help = "Emails password reset instructions to all users"
+
+    def handle(self, *args, **kwargs):
+        users = User.objects.filter(password="")
+        for user in users:
+            try:
+                if user.email:
+                    print("Sending email for to this email:", user.email)
+                    form = PasswordResetForm({"email": user.email})
+                    assert form.is_valid()
+                    request = HttpRequest()
+                    request.META["SERVER_NAME"] = "marking.councilclimatescorecards.uk"
+                    request.META["SERVER_PORT"] = 443
+                    form.save(
+                        request=request,
+                        domain_override="marking.councilclimatescorecards.uk",
+                        use_https=True,
+                        from_email="admin@mysite.com",
+                        subject_template_name="registration/initial_password_email_subject.txt",
+                        email_template_name="registration/initial_password_email.html",
+                    )
+            except Exception as e:
+                print(e)
+                continue
+
+        return "done"
