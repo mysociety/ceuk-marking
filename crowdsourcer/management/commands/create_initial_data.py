380e9d0f42a345508da85d52932a61810d5b31d0
Struan Donald <struan@exo.org.uk>
2023-02-21 16:47:24 +0000

    update council import to set do_not_mark flag

--- crowdsourcer/management/commands/create_initial_data.py
+++ crowdsourcer/management/commands/create_initial_data.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
+
+import pandas as pd
 
 from crowdsourcer.models import PublicAuthority, QuestionGroup, ResponseType, Section
 from utils import mapit
@@ -6,6 +9,8 @@ from utils import mapit
 
 class Command(BaseCommand):
     help = "set up authorities and question groups"
+
+    do_not_mark_file = settings.BASE_DIR / "data" / "do_not_mark.csv"
 
     groups = ["Single Tier", "District", "County", "Northern Ireland"]
 
@@ -30,6 +35,10 @@ class Command(BaseCommand):
             "-q", "--quiet", action="store_true", help="Silence progress bars."
         )
 
+    def get_do_not_mark_list(self):
+        df = pd.read_csv(self.do_not_mark_file)
+        return list(df["gss-code"])
+
     def get_group(self, props):
         group = "District"
 
@@ -49,6 +58,8 @@ class Command(BaseCommand):
         return g
 
     def handle(self, quiet: bool = False, *args, **options):
+        do_not_mark_list = self.get_do_not_mark_list()
+
         for section in self.sections:
             c, c = Section.objects.get_or_create(title=section)
 
@@ -66,10 +77,15 @@ class Command(BaseCommand):
         if not quiet:
             print("Importing Areas")
         for area in areas:
+            do_not_mark = False
+            if area["codes"]["gss"] in do_not_mark_list:
+                do_not_mark = True
+
             a, created = PublicAuthority.objects.update_or_create(
                 unique_id=area["codes"]["gss"],
                 defaults={
                     "name": self.name_map.get(area["name"], area["name"]),
                     "questiongroup": self.get_group(area),
+                    "do_not_mark": do_not_mark,
                 },
             )
