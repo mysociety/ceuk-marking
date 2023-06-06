498476af59d781d7436932a39da4ce5cd50797d6
Alexander Griffen <alexander@mysociety.org>
2023-06-06 10:21:43 +0100

    fix: set marker objects as auditors

--- crowdsourcer/management/commands/activate_audit_volunteers.py
+++ crowdsourcer/management/commands/activate_audit_volunteers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
+from crowdsourcer.models import Assigned, Marker, ResponseType
+
 YELLOW = "\033[33m"
 NOBOLD = "\033[0m"
 
@@ -15,6 +17,19 @@ class Command(BaseCommand):
         if not kwargs["commit"]:
             self.stdout.write(
                 f"{YELLOW}Not commiting changes. Call with --commit to update database{NOBOLD}"
+            )
+        users_to_be_made_auditors = (
+            Assigned.objects.filter(response_type__type="Audit")
+            .values_list("user", flat=True)
+            .distinct()
+        )
+        self.stdout.write(
+            f"{len(users_to_be_made_auditors) - User.objects.filter(marker__response_type__type='Audit').count()} users to be made into auditors."
+        )
+        if kwargs["commit"]:
+            rt = ResponseType.objects.get(type="Audit")
+            Marker.objects.filter(user__id__in=users_to_be_made_auditors).update(
+                response_type=rt
             )
 
         users = User.objects.filter(
