f527fb64abfefa981490adafd1690165a6dbfe28
Struan Donald <struan@exo.org.uk>
2026-04-08 16:26:03 +0100

    [CEUK] add a command to update council homes automatic points

---
+++ crowdsourcer/management/commands/automatic_no_council_homes_points.py
@@ -0,0 +1,386 @@
+from collections import defaultdict
+
+from django.contrib.auth.models import User
+
+import pandas as pd
+
+from crowdsourcer.import_utils import BaseImporter
+from crowdsourcer.models import (
+    MarkingSession,
+    Option,
+    PublicAuthority,
+    Question,
+    Response,
+    ResponseType,
+)
+
+
+class Command(BaseImporter):
+    help = "Add automatic points"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-q", "--quiet", action="store_true", help="Silence debug text."
+        )
+
+        parser.add_argument(
+            "--session",
+            action="store",
+            required=True,
+            help="Marking session to use questions with",
+        )
+
+        parser.add_argument(
+            "--previous",
+            action="store",
+            required=True,
+            help="Previous marking session to copy answers from",
+        )
+
+        parser.add_argument(
+            "--file",
+            action="store",
+            required=True,
+            help="CSV file containing the automatic points",
+        )
+
+        parser.add_argument(
+            "--stage",
+            action="store",
+            required=True,
+            help="marking stage to add responses to",
+        )
+
+        parser.add_argument(
+            "--option_map",
+            action="store",
+            help="CSV file containing option mapping from previous answers",
+        )
+
+        parser.add_argument(
+            "--update_existing_responses",
+            action="store_true",
+            help="Always update existings responses",
+        )
+
+        parser.add_argument(
+            "--commit", action="store_true", help="Commits changes to DB"
+        )
+
+    def get_points(self, file):
+        df = pd.read_csv(file)
+        df["answer in GRACE"] = df["answer in GRACE"].astype(str)
+
+        return df
+
+    def get_option_map(self, file):
+        if file == "" or file is None:
+            return {}
+
+        df = pd.read_csv(file)
+        df.question = df.question.astype(str)
+
+        option_map = defaultdict(dict)
+        for _, option in df.iterrows():
+            if option_map[option["section"]].get(option["question"]) is None:
+                option_map[option["section"]][option["question"]] = {}
+
+            option_map[option["section"]][option["question"]][option["prev_option"]] = (
+                option["new_option"]
+            )
+
+        return option_map
+
+    def scrub_council_type(self, types):
+        type_map = {
+            "COMB": "COMB",
+            "CTY": "CTY",
+            "LGD": "LGD",
+            "MD": "MTD",
+            "MTD": "MTD",
+            "UTA": "UTA",
+            "COI": "COI",
+            "NMD": "NMD",
+            "DIS": "DIS",
+            "CC": "LBO",
+            "LBO": "LBO",
+            "SCO": "UTA",
+            "WPA": "UTA",
+            "NID": "UTA",
+            "UA": "UTA",
+            "SRA": "COMB",
+        }
+        scrubbed = []
+        for t in types:
+            t = t.strip()
+            if type_map.get(t) is not None:
+                scrubbed.append(type_map[t])
+            else:
+                self.print_error(f"bad council type {t}")
+        return scrubbed
+
+    def get_mapped_answer(self, answer, q, answer_map):
+        if (
+            answer_map.get(q.section.title) is not None
+            and answer_map[q.section.title].get(q.number_and_part) is not None
+            and answer_map[q.section.title][q.number_and_part].get(answer) is not None
+        ):
+            return answer_map[q.section.title][q.number_and_part][answer]
+
+        return answer
+
+    def handle(
+        self,
+        quiet: bool = False,
+        commit: bool = False,
+        file: str = "",
+        stage: str = "",
+        session: str = "",
+        option_map: str = "",
+        update_existing_responses: bool = False,
+        *args,
+        **kwargs,
+    ):
+        self.quiet = quiet
+
+        u, _ = User.objects.get_or_create(
+            username="Auto_point_script",
+        )
+
+        try:
+            rt = ResponseType.objects.get(type=stage)
+            prev_rt = ResponseType.objects.get(type="Audit")
+        except ResponseType.DoesNotExist:
+            self.stderr.write(f"No such ResponseType {stage}")
+
+        try:
+            ms = MarkingSession.objects.get(label=session)
+        except MarkingSession.DoesNotExist:
+            self.stderr.write(f"No such Marking Session {session}")
+
+        points = self.get_points(file)
+        answer_map = self.get_option_map(option_map)
+
+        for _, point in points.iterrows():
+            if pd.isna(point["question number"]):
+                self.stderr.write(
+                    f"Bad value for question number {point['question number']} in row {_}"
+                )
+                continue
+
+            responses_added = 0
+            responses_overidden = 0
+            existing_responses = 0
+
+            if point["section"] == "":
+                continue
+
+            c_args = {}
+
+            if (
+                point.get("council country", None) is not None
+                and pd.isna(point["council country"]) is False
+            ):
+                countries = point["council country"].strip()
+                if countries != "":
+                    countries = countries.split(",")
+                    c_args["country__in"] = [c.lower() for c in countries]
+
+            councils = PublicAuthority.objects.filter(marking_session=ms, **c_args)
+
+            q_args = {"number": point["question number"]}
+            if (
+                not pd.isna(point["question part"])
+                and point.get("question part", None) is not None
+            ):
+                q_args["number_part"] = point["question part"].strip()
+
+            try:
+                question = Question.objects.get(
+                    section__marking_session=ms,
+                    section__title=point["section"],
+                    **q_args,
+                )
+            except Question.DoesNotExist:
+                self.print_error(
+                    f"no matching question for {point['section']}, {q_args}"
+                )
+                continue
+            except Question.MultipleObjectsReturned:
+                self.print_error(
+                    f"multiple matching questions for {point['section']}, {q_args}"
+                )
+                continue
+
+            copy_last_year = True
+            for council in councils:
+                add_response = False
+                override_response = False
+
+                options = None
+                try:
+                    prev_response = Response.objects.get(
+                        question=question.previous_question,
+                        authority=council,
+                        response_type=prev_rt,
+                    )
+                except Response.DoesNotExist:
+                    self.print_error(
+                        f"no previous response exists for {council.name} for {question.number_and_part}, {question.section.title}"
+                    )
+                    continue
+                except Response.MultipleObjectsReturned:
+                    self.print_error(
+                        f"multiple previous responses exist for {council.name} for {question.number_and_part}, {question.section.title}"
+                    )
+                    continue
+
+                allowed_answers = [
+                    "Council directly owns or manages less than 100 homes so question doesn't apply",
+                    "Council does not directly own or manage social housing, likely to provide social housing through external organisations",
+                ]
+                try:
+                    if question.question_type == "multiple_choice":
+                        options = []
+                        for opt in prev_response.multi_option.all():
+                            if opt.description not in allowed_answers:
+                                continue
+                            answer = self.get_mapped_answer(
+                                opt.description, question, answer_map
+                            )
+                            new_opt = Option.objects.get(
+                                question=question, description=answer
+                            )
+                            options.append(new_opt)
+                    else:
+                        if prev_response.option.description not in allowed_answers:
+                            continue
+                        answer = self.get_mapped_answer(
+                            prev_response.option.description, question, answer_map
+                        )
+                        option = Option.objects.get(
+                            question=question, description=answer
+                        )
+                except Option.DoesNotExist:
+                    self.print_error(
+                        f"no matching option for {question.number_and_part}, {point['section']} - '{prev_response.option.description}'"
+                    )
+                    continue
+                except AttributeError as e:
+                    self.print_error(
+                        f"Problem getting previous answer for {question.number_and_part}, {point['section']} - '{e}'"
+                    )
+                    continue
+
+                try:
+                    response = Response.objects.get(
+                        question=question, authority=council, response_type=rt
+                    )
+                    if question.question_type == "multiple_choice":
+                        if response.multi_option is not None:
+                            options = [x.id for x in response.multi_option.all()]
+                            if option.id not in options:
+                                self.print_info(
+                                    f"existing response does not contain expected response for {question.number_and_part}, {point['section']}, {council.name}"
+                                )
+                    else:
+                        if response.option != option:
+                            self.print_info(
+                                f"different existing response for {question.number_and_part}, {point['section']}, {council.name}"
+                            )
+                        if point.get("override_response", None) is not None:
+                            override_response = True
+                    self.print_info(
+                        f"response exists for {council.name} for {question.number_and_part}, {question.section.title}"
+                    )
+                    existing_responses += 1
+                except Response.DoesNotExist:
+                    add_response = True
+
+                response_opts = {
+                    "user": u,
+                    "question": question,
+                    "authority": council,
+                    "response_type": rt,
+                    "private_notes": "Automatically assigned mark",
+                }
+                if copy_last_year:
+                    response_opts["public_notes"] = prev_response.public_notes
+                    response_opts["page_number"] = prev_response.page_number
+                    response_opts["evidence"] = prev_response.evidence
+                    response_opts["private_notes"] = (
+                        prev_response.private_notes + "\nAutomatically assigned mark"
+                    )
+
+                    if pd.isna(point["evidence notes"]) is False:
+                        response_opts["evidence"] = point["evidence notes"]
+                    if pd.isna(point["evidence link"]) is False:
+                        response_opts["public_notes"] = point["evidence link"]
+                else:
+                    if pd.isna(point["page no"]) is False:
+                        response_opts["page_number"] = point["page no"]
+                    if pd.isna(point["evidence link"]) is False:
+                        response_opts["public_notes"] = point["evidence link"]
+                    if pd.isna(point["evidence notes"]) is False:
+                        response_opts["evidence"] = point["evidence notes"]
+                    if (
+                        pd.isna(point["private notes"]) is False
+                        and point["private notes"] != "n/a"
+                    ):
+                        response_opts["private_notes"] = (
+                            str(point["private notes"])
+                            + "\nAutomatically assigned mark"
+                        )
+
+                if add_response:
+                    responses_added += 1
+                    self.print_debug(
+                        f"creating response for {council.name} for {question.number_and_part}, {question.section.title}"
+                    )
+
+                    if commit:
+                        if question.question_type == "multiple_choice":
+                            r = Response.objects.create(**response_opts)
+                            if options is not None:
+                                for o in options:
+                                    r.multi_option.add(o)
+                            else:
+                                r.multi_option.add(option.id)
+                        else:
+                            response_opts["option"] = option
+                            r = Response.objects.create(**response_opts)
+
+                elif override_response or update_existing_responses:
+                    responses_overidden += 1
+                    self.print_info(
+                        f"overriding response for {council.name} for {question.number_and_part}, {question.section.title}"
+                    )
+
+                    if question.question_type != "multiple_choice":
+                        response.option = option
+
+                    response.private_notes = (
+                        response_opts["private_notes"]
+                        + "\n"
+                        + "Overridden by automatic assignment"
+                    )
+
+                    response.public_notes = response_opts["public_notes"]
+                    response.evidence = response_opts["evidence"]
+                    response.page_number = response_opts["page_number"]
+
+                    if commit:
+                        response.save()
+                        if question.question_type == "multiple_choice":
+                            response.multi_option.clear()
+                            if options is not None:
+                                for o in options:
+                                    response.multi_option.add(o)
+                            else:
+                                response.multi_option.add(option.id)
+
+            self.print_success(
+                f"Added {responses_added} responses for {question.section.title} {question.number_and_part}, {existing_responses} existing responses, {responses_overidden} responses overridden",
+            )
+        if not commit:
+            self.print_info("call with --commit to commit changed to database")
